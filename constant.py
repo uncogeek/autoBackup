3d19989e103b1d75c065f870be4b35a3341d7a6f
Ehsan Heydari <80108440+uncogeek@users.noreply.github.com>
2024-07-27 13:17:58 +0330

    Update constant.py

--- constant.py
+++ constant.py
@@ -1,20 +1,20 @@
 # FTP account config
-FTP_SERVER = "ftp.stre.ir"
-FTP_USER = "python@stre.ir"
-FTP_PASSWORD = "sUs13rs0h_@A"
+FTP_SERVER = "ftp.example.com"
+FTP_USER = "user@example.com"
+FTP_PASSWORD = "ftppassword"
 
 # Email config
-EMAIL_USER = 'uncomailer@gmail.com'
-EMAIL_PASSWORD = 'qzbmmlzaovngcpuy'
+EMAIL_USER = 'you@gmail.com'
+EMAIL_PASSWORD = 'app password'
 EMAIL_SMTP_ADDRESS = 'smtp.gmail.com'
 EMAIL_SMTP_PORT = 587
-EMAIL_ADRRESS_TO = "sooterex@gmail.com"
+EMAIL_ADRRESS_TO = "to@gmail.com"
 
 #etc
-BACKUP_DESTINATION_BACKUP_FOLDER = r'C:\Users\Lucifer\Desktop\tmp\temp\copy_file\backup_path\\'
+BACKUP_DESTINATION_BACKUP_FOLDER = r'C:\Users\You\Desktop\backup_path\\'
 
-BACKUP_SOURCEFOLDERS_TO_BACKUP = [r'C:\Users\Lucifer\Desktop\tmp\temp\copy_file\admin\\',
-                  r'C:\Users\Lucifer\Desktop\tmp\temp\copy_file\gate\\',
-                  r'C:\Users\Lucifer\Desktop\tmp\temp\copy_file\software_admin\\']
+BACKUP_SOURCEFOLDERS_TO_BACKUP = [r'C:\Users\You\Desktop\copy_file\admin\\',
+                  r'C:\Users\You\Desktop\copy_file\gate\\',
+                  r'C:\Users\You\Desktop\copy_file\software_admin\\']
 
 BACKUP_FOLDER_NAMES_TO_BACKUP = ['admin', 'gate', 'software_admin']
